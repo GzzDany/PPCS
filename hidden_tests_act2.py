0b278795e37d1ae9d69e0bc6d1c58c9adae0d363
GzzDany <60372312+GzzDany@users.noreply.github.com>
2025-06-13 14:51:08 -0600

    Update hidden_tests_act2.py

--- hidden_tests_act2.py
+++ hidden_tests_act2.py
@@ -6,3 +6,12 @@ def input_formula_cuadratica_suma(num_tests=15):
   from random import choices
   input_args = [{"a":a, "b":b, "c":c} for a, b, c in zip(choices(range(1,10), k=num_tests), choices(range(1,10), k=num_tests), choices(range(1,10), k=num_tests))]
   return input_values, input_args
+
+def sol_calcular_hipotenusa(cateto1, cateto2):
+  return (cateto1**2 + cateto2**2)**0.5
+
+def input_calcular_hipotenusa(num_tests=15):
+  input_values = [[]]*num_tests
+  from random import choices 
+  input_args = [{"cateto1":cateto1, "cateto2":cateto2} for cateto1, cateto2 in zip(choices(range(1, 100), k=num_tests), choices(range(1, 100), k=num_tests))]
+  return input_values, input_args
